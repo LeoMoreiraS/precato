83b2848929819ed12669d93193e1e272a8fa96ed
Leonardo Moreira <leomoreira.com@gmail.com>
2021-11-04 12:46:44 -0300

    chore: add eslint and tsconfig configs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express from "express";
 import dotenv from "dotenv";
-dotenv.config()
+import express from "express";
+
+dotenv.config();
+
 const app = express();
 
-app.listen(process.env.PORT,()=>{
-    console.log("listening on port " + process.env.PORT)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+});
