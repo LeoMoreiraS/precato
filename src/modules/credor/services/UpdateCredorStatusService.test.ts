2c2da25f29154649ef17d97f57d8c226842f1bd6
Leonardo Moreira <leomoreira.com@gmail.com>
2021-11-05 15:31:31 -0300

    test: ensure credor status is updated correctly

--- src/modules/credor/services/UpdateCredorStatusService.test.ts
+++ src/modules/credor/services/UpdateCredorStatusService.test.ts
@@ -6,16 +6,26 @@ import { ICreateCredorDTO, ICredorRepository } from "..";
 
 import { IUpdateStatusCredorDTO } from "../DTOs/IUpdateStatusCredorDTO";
 import { Credor } from "../entities/Credor";
-import { CreateCredorService } from "./CreateCredorService";
+import { UpdateCredorStatusService } from "./UpdateCredorStatusService";
 
 const date = new Date();
 const valid_cpf = cpfValidator.generate();
-const duplicated_cpf = "126.229.456-80";
+
 @injectable()
 class StubRepository implements ICredorRepository {
-    updateStatus({ status, cpf }: IUpdateStatusCredorDTO): Promise<Credor> {
-        console.log(status, cpf);
-        throw new Error("Method not implemented.");
+    async updateStatus({
+        status,
+        cpf,
+    }: IUpdateStatusCredorDTO): Promise<Credor> {
+        const credor: Credor = await {
+            id: "valid_uuid",
+            cpf,
+            name: "credor_name",
+            status,
+            updated_at: date,
+            created_at: date,
+        };
+        return credor;
     }
     async create({ name, cpf }: ICreateCredorDTO): Promise<Credor> {
         const credor: Credor = await {
@@ -29,7 +39,7 @@ class StubRepository implements ICredorRepository {
         return credor;
     }
     async findByCpf(cpf: string): Promise<Credor> {
-        if (cpf === duplicated_cpf) {
+        if (cpf === valid_cpf) {
             const credor = await new Credor();
             return credor;
         }
@@ -37,63 +47,70 @@ class StubRepository implements ICredorRepository {
     }
 }
 
-describe("CreateCredorService tests", () => {
+describe("UpdateCredorStatusService tests", () => {
     beforeEach(() => {
         container.clearInstances();
     });
     test("Should call execute function with correct params", () => {
-        const createCredorService = container
+        const updateCredorStatusService = container
             .createChildContainer()
             .register<ICredorRepository>("CredorRepository", StubRepository)
-            .resolve(CreateCredorService);
-        const spyService = jest.spyOn(createCredorService, "execute");
-        createCredorService.execute({ name: "valid_name", cpf: valid_cpf });
+            .resolve(UpdateCredorStatusService);
+        const spyService = jest.spyOn(updateCredorStatusService, "execute");
+        updateCredorStatusService.execute({ approval: true, cpf: valid_cpf });
         expect(spyService).toBeCalledWith({
-            name: "valid_name",
+            approval: true,
             cpf: valid_cpf,
         });
     });
     test("Should throw if a cpf is invalid", () => {
-        const createCredorService = container
+        const updateCredorStatusService = container
             .createChildContainer()
             .register<ICredorRepository>("CredorRepository", StubRepository)
-            .resolve(CreateCredorService);
-        const spyService = jest.spyOn(createCredorService, "execute");
-        createCredorService.execute({
-            name: "valid_name",
+            .resolve(UpdateCredorStatusService);
+        const spyService = jest.spyOn(updateCredorStatusService, "execute");
+        updateCredorStatusService.execute({
+            approval: true,
             cpf: "123.456.789-80",
         });
         expect(spyService).toThrow();
     });
-    test("Should return correct values", async () => {
-        const createCredorService = container
+    test("Should return correct values if credor is aproved", async () => {
+        const updateCredorStatusService = container
             .createChildContainer()
             .register<ICredorRepository>("CredorRepository", StubRepository)
-            .resolve(CreateCredorService);
-        const credor = await createCredorService.execute({
-            name: "valid_name",
+            .resolve(UpdateCredorStatusService);
+        const credor = await updateCredorStatusService.execute({
+            approval: true,
             cpf: valid_cpf,
         });
         console.log(credor);
         expect(credor).toEqual({
             id: "valid_uuid",
-            name: "valid_name",
+            name: "credor_name",
             cpf: valid_cpf,
-            status: "created",
+            status: "Aproved",
             created_at: date,
             updated_at: date,
         });
     });
-    test("Should not create credor if a credor cpf already exists", () => {
-        const createCredorService = container
+    test("Should return correct values if credor is rejected", async () => {
+        const updateCredorStatusService = container
             .createChildContainer()
             .register<ICredorRepository>("CredorRepository", StubRepository)
-            .resolve(CreateCredorService);
-        const spyService = jest.spyOn(createCredorService, "execute");
-        createCredorService.execute({
-            name: "valid_name",
-            cpf: duplicated_cpf,
+            .resolve(UpdateCredorStatusService);
+        const credor = await updateCredorStatusService.execute({
+            approval: false,
+            cpf: valid_cpf,
         });
-        expect(spyService).toThrow();
+        console.log(credor);
+        expect(credor).toEqual({
+            id: "valid_uuid",
+            name: "credor_name",
+            cpf: valid_cpf,
+            status: "Rejected",
+            created_at: date,
+            updated_at: date,
+        });
     });
 });
