3694e0c528fca0f1bc5ca24bfe4ec4caa9ab6444
Leonardo Moreira <leomoreira.com@gmail.com>
2021-11-07 16:49:40 -0300

    fix: eslint format

--- src/database/database.test.ts
+++ src/database/database.test.ts
@@ -3,7 +3,7 @@ import "./index";
 import { getConnection } from "typeorm";
 
 test("should match connection data", () => {
-  const connection = getConnection();
-  console.log(connection.options);
-  expect(connection.options.database).toBe("precato");
+    const connection = getConnection();
+    console.log(connection.options);
+    expect(connection.options.database).toBe("precato");
 });
